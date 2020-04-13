d960d47d8e91e58a6bca010e62280f037ae81d57
Agustin Ramis <aramis@eventbrite.com>
2020-04-13 11:09:55 -0300

    Test: Login

--- todolist_app/tests.py
+++ todolist_app/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class TestTodoList(TestCase):
+
+    def test_login(self):
+        c = Client()
+        response = c.post('/login', {
+            'username': 'aramis', 'password': 'agustin20'
+            })
+        self.assertEquals(response.status_code, 200)
+         
