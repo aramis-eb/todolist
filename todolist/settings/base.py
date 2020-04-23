3f5290028dea7a91bff77e28a73e4892cae18d1a
Agustin Ramis <aramis@eventbrite.com>
2020-04-23 16:28:41 -0300

    fix

--- todolist/settings/base.py
+++ todolist/settings/base.py
@@ -119,7 +119,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 PAGINATION_SETTINGS = {
     'PAGE_RANGE_DISPLAYED': 10,
