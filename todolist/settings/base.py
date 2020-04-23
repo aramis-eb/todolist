9e567d82ecb88186cd42719f5e97277a7437e04e
Agustin Ramis <aramis@eventbrite.com>
2020-04-23 17:01:38 -0300

    fix settings to heroku

--- todolist/settings/base.py
+++ todolist/settings/base.py
@@ -119,7 +119,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_prod')
 PAGINATION_SETTINGS = {
     'PAGE_RANGE_DISPLAYED': 10,
     'MARGIN_PAGES_DISPLAYED': 2,
